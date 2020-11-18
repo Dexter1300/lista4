6819275dde716908c12a639fc70036ef343eb2da
Dexter1300 <127375@student.uni.opole.pl>
2020-11-18 11:58:26 +0100

    Dodanie serializacji przyciskow i nowych okien

--- lista4/lista4/MainWindow.xaml.cs
+++ lista4/lista4/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace lista4
 {
@@ -20,9 +23,55 @@ namespace lista4
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Student> listastud = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitBinding();
+        }
+
+        private void InitBinding()
+        {
+            listastud = new List<Student>();
+            try
+            {
+                using (var reader = new StreamReader("ListaStudentow.xml"))
+                {
+                    XmlSerializer deserializer = new XmlSerializer(typeof(List<Student>),
+                        new XmlRootAttribute("ArrayOfPerson"));
+                    listastud = (List<Student>)deserializer.Deserialize(reader);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Brak pliku do załadowania!", "Uwaga", MessageBoxButton.OK);
+            }
+            lstPersons.ItemsSource = listastud;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            win2 window2 = new win2();
+            window2.Show();
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            Window3 win3 = new Window3(id.Text);
+            win3.Show();
+        }
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            InitBinding();
+        }
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            var serializer = new XmlSerializer(listastud.GetType());
+            using (var writer = XmlWriter.Create("PersonList.xml"))
+            {
+                serializer.Serialize(writer, listastud);
+            }
         }
     }
 }
